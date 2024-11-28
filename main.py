27fd298993dc9f36c2135a4f5a3f515707ddbccb
code-with-srk <surajkamble01997@gmail.com>
2024-11-28 21:33:28 +0530

    task d

---
+++ main.py
@@ -0,0 +1,14 @@
+class firstclass:
+    val1 = 0
+    val2 = 0
+
+
+    def __init__(self):
+        print('this is constructor')
+        
+
+f1 = firstclass()
+
+print('f1.val1 = ', f1.val1)
+print('f1.val2 = ', f1.val2)
+                  
